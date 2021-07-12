9607aac315178c5dac880cf4263885a7db23fd7c
android <22prachi@gmail.com>
2021-07-12 11:18:00 +0530

    12/07/21

---
+++ app/src/main/java/com/wizebrain/adebdriver/activity/DrivingLicenseActivity.kt
@@ -0,0 +1,12 @@
+package com.wizebrain.adebdriver.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wizebrain.adebdriver.R
+
+class DrivingLicenseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driving_license)
+    }
+}
---
+++ app/src/main/java/com/wizebrain/adebdriver/activity/RateUserActivity.kt
@@ -0,0 +1,12 @@
+package com.wizebrain.adebdriver.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wizebrain.adebdriver.R
+
+class RateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate_user)
+    }
+}
